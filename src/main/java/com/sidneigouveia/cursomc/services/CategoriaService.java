b186009a278536820442fea9a5c3e92cf7532411
SidneiGouveia <gouveiajunior7@gmail.com>
2024-09-30 21:44:51 -0300

    Ajustes no endpoint/categorias/id

--- src/main/java/com/sidneigouveia/cursomc/services/CategoriaService.java
+++ src/main/java/com/sidneigouveia/cursomc/services/CategoriaService.java
@@ -7,20 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.sidneigouveia.cursomc.domain.Categoria;
 import com.sidneigouveia.cursomc.repositories.CategoriaRepository;
+import com.sidneigouveia.cursomc.services.exceptions.ObjectNotFoundException; // Use sua exceção customizada
 
 @Service
 public class CategoriaService {
 
-	@Autowired
+    @Autowired
     private CategoriaRepository repo;
-	
-	
-	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
- 	}
 
-
-
-			
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
 }
