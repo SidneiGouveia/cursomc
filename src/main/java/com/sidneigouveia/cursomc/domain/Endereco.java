f3987546e4bb203011680adf7b5dd9bcb8954aa3
SidneiGouveia <gouveiajunior7@gmail.com>
2024-10-02 14:21:39 -0300

    commit Endpoint

--- src/main/java/com/sidneigouveia/cursomc/domain/Endereco.java
+++ src/main/java/com/sidneigouveia/cursomc/domain/Endereco.java
@@ -3,6 +3,8 @@ package com.sidneigouveia.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,7 +28,7 @@ public class Endereco implements Serializable {
 	private String complemento;
 	private String bairro;
 	private String cep;
-	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="Cliente_id")
 	private Cliente cliente;
